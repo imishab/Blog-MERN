9a8d30deec2a72f5f9a10533cbeeb6b4f42499d6
Mishab <mishabmsb91@gmail.com>
2024-05-19 10:26:38 +0530

    routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 export default function App() {
   return (
-    <div className="text-3xl text-red-500">App</div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/signin' element={<Signin/>} />
+
+     </Routes>
+    </BrowserRouter>
   )
 }
